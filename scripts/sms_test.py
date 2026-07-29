0d962017290fd44f946a700aab9ff4447753fdfe
Elisha Akabua Nunana <nunanaelisha2004@gmail.com>
2026-07-29 20:39:43 +0000

    Changed caregiver phone number

--- scripts/sms_test.py
+++ scripts/sms_test.py
@@ -8,7 +8,7 @@ pathway of the project is proven functional.
 import serial
 import time
 
-CAREGIVER_PHONE = "+233570001227"
+CAREGIVER_PHONE = "+233552427345"
 SERIAL_DEVICE = "/dev/serial0"
 BAUD_RATE = 9600
 
